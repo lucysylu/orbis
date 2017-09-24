0258757fb8e791cfcc3c95e018a1ac77019593b9
Javed Nissar <javednissar@gmail.com>
2017-09-23 21:37:50 -0400

    added various utility methods

--- PlayerAI.py
+++ PlayerAI.py
@@ -16,6 +16,16 @@ class PlayerAI:
         self.scouts = []
         self.closed = False
         self.hunter_nest_pairs = {}
+        self.spawned = []
+        self.nest_spawn = {}
+        self.move_count = 0
+        self.goal_nests = 3
+        self.is_early_game = True
+        self.friendly_nest_distance_importance = 0.25
+        self.enemy_nest_distance_importance = 0.25
+        self.nest_core_importance = 0.5
+        self.target_nests = {}
+        self.defense_points = {}
 
     def check_walls(self, world):
 
@@ -82,6 +92,87 @@ class PlayerAI:
         next_point = world.get_next_point_in_shortest_path(self.hunter_pair[unit.uuid])
         world.move(unit, next_point)
 
+    def nest_fitness(self, friendly_nest_distance, enemy_nest_distance, nesting_core_score, world):
+        return (self.friendly_nest_distance_importance * -friendly_nest_distance +
+            self.enemy_nest_distance_importance * enemy_nest_distance +
+            self.nest_core_importance * nesting_core_score)
+
+    def obtain_best_nest_points(self, world):
+        friendly_nest_position = world.get_friendly_nest_positions()[0]
+        enemy_nest_position = world.get_enemy_nest_positions()[0]
+        best_nests = []
+
+        for tile in world.get_tiles():
+            tile_position = tile.position
+
+            friendly_distance = world.get_shortest_path_distance(tile_position, friendly_nest_position)
+            enemy_distance = world.get_shortest_path_distance(tile_position, enemy_nest_position)
+            nesting_core_score = 0
+            if self.is_nesting_core(world, point):
+                nesting_core_score = 1
+            best_nests.append((tile, nest_fitness(friendly_distance,
+                                                  enemy_distance,
+                                                  nesting_core_score)))
+
+        best_nests = sorted(best_nests, key=lambda x:x(1))
+        overall_best_nests = best_nests[0:self.goal_nests]
+        best_nest_dict = {}
+
+        for nest in overall_best_nests:
+            nearby_tiles = world.get_neighbours(nest.position)
+            best_nest_dict[nest] = nearby_tiles
+
+        return best_nest_dict
+
+    def get_northwestern_point(self, world, point, neighbors):
+        northern_point = neighbors[Direction.NORTH]
+        western_point = neighbors[Direction.WEST]
+
+        if world.at_edge(point) and world.at_edge(northern_point):
+            return (northern_point[0] - 1, northern_point[1])
+        else:
+            return (western_point[0], western_point[1] + 1)
+
+    def get_northeastern_point(self, world, point, neighbors):
+        northern_point = neighbors[Direction.NORTH]
+        eastern_point = neighbors[Direction.EAST]
+
+        if world.at_edge(point) and world.at_edge(northern_point):
+            return (northern_point[0] + 1, northern_point[1])
+        else:
+            return (eastern_point[0], eastern_point[1])
+
+    def get_southwestern_point(self, world, point, neighbors):
+        southern_point = neighbors[Direction.SOUTH]
+        western_point = neighbors[Direction.WEST]
+
+        if world.at_edge(point) and world.at_edge(southern_point):
+            return (southern_point[0] - 1, southern_point[1])
+        else:
+            return (western_point[0], western_point[1] - 1)
+
+    def get_southeastern_point(self, world, point, neighbors):
+        southern_point = neighbors[Direction.SOUTH]
+        eastern_point = neighbors[Direction.EAST]
+
+        if world.at_edge(point) and world.at_edge(southern_point):
+            return (southern_point[0] + 1, southern_point[1])
+        else:
+            return (eastern_point[0], eastern_point[1] - 1)
+
+    def obtain_defence_points(self, world):
+        friendly_nest_positions = world.get_friendly_nest_positions()
+        for friendly_nest_position in friendly_nest_positions:
+            neighbors = world.get_neighbours(friendly_nest_position)
+
+            northwestern_point = self.get_northwestern_point(world, friendly_nest_position, neighbors)
+            northeastern_point = self.get_northeastern_point(world, friendly_nest_position, neighbors)
+            southwestern_point = self.get_southwestern_point(world, friendly_nest_position, neighbors)
+            southeastern_point = self.get_southeastern_point(world, friendly_nest_position, neighbors)
+
+            self.defense_points[friendly_nest_position] = [northwestern_point, northeastern_point, southwestern_point,
+                                                           southeastern_point]
+
 
 
     def do_move(self, world, friendly_units, enemy_units):
@@ -96,10 +187,30 @@ class PlayerAI:
         # Build thou nests
         # Grow, become stronger
         # Take over the world
+        move_count += 1
         for unit in friendly_units:
             path = world.get_shortest_path(unit.position,
                                            world.get_closest_capturable_tile_from(unit.position, None).position,
                                            None)
+            #assign a new firefly to a role
+            if unit.uuid not in self.spawned:
+                #TODO: Add code for checking if merged
+                if (self.is_early_game and self.move_count % 2 == 0):
+                    self.scouts.append(unit.uuid)
+                elif (not self.is_early_game and self.move_count % 2 == 0):
+                    self.hunters.append(unit.uuid)
+                else:
+                    self.defenders.append(unit.uuid)
+                self.spawned.append(unit.uuid)
+
+            if unit.uuid in self.spawned:
+                if unit.uuid in self.hunters:
+
+                elif unit.uuid in self.scouts:
+
+                elif unit.uuid in self.defenders:
+
+
 
             if path: world.move(unit, path[0])
 
